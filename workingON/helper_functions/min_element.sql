db1a7177f4d43695eafac01ba0e1ce3d1cd7929c
Radyko <122955109+Radyko@users.noreply.github.com>
2025-07-14 07:01:40 -0400

    Organizational changes.

--- workingON/helper_functions/min_element.sql
+++ workingON/helper_functions/min_element.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION upper_bound(set1 int4range, set2 int4range)
+CREATE OR REPLACE FUNCTION lower_bound(set1 int4range, set2 int4range)
 RETURNS int AS $$
 BEGIN
     RETURN 
@@ -8,8 +8,8 @@ $$ LANGUAGE plpgsql;
 
 
 -- -- 
--- SELECT upper_bound('[1,7]'::int4range, '[3,4]'::int4range) AS test1;
--- SELECT upper_bound('empty'::int4range, '[3,9]'::int4range) AS test2;
--- SELECT upper_bound(NULL::int4range, '[3,9]'::int4range) AS test3;
--- SELECT upper_bound(NULL::int4range, NULL::int4range) AS test4;
--- SELECT upper_bound('empty'::int4range, 'empty'::int4range) AS test5; -- maybe rv 'empty' and not null
+-- SELECT lower_bound('[1,7]'::int4range, '[3,4]'::int4range) AS test1;
+-- SELECT lower_bound('empty'::int4range, '[3,9]'::int4range) AS test2;
+-- SELECT lower_bound(NULL::int4range, '[3,9]'::int4range) AS test3;
+-- SELECT lower_bound(NULL::int4range, NULL::int4range) AS test4;
+-- SELECT lower_bound('empty'::int4range, 'empty'::int4range) AS test5; -- maybe rv 'empty' and not null
