4e65961f0841e2c7a121403fea59256b7072b964
asxvi <astanson02@gmail.com>
2026-01-29 08:00:42 -0600

    need to add reprod randomness

--- c_extension/tests/main.py
+++ c_extension/tests/main.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import psycopg2
+import psycopg2.extras
 from configparser import ConfigParser
 from dataclasses import dataclass
 import numpy as np
@@ -19,6 +20,7 @@ class ExperimentSettings:
     num_trials: int
     dataset_size: int
     uncertain_ratio: float              #uncertainty can be thru mult 0, or missing vals in col.. (more?). we will generate both cases
+    mult_size_range: tuple
     interval_size_range: tuple
     num_intervals_range: tuple
     num_intervals: int
@@ -39,28 +41,18 @@ class ExperimentRunner:
         
         trial_results = []
         
-        for trial in range(experiment.num_trials):    
-            self.generate_data(experiment, trial)
+        with self.connect_db() as conn:
+            with conn.cursor() as cur:
+                for trial in range(experiment.num_trials):    
+                    # seed_str = f"{experiment.name}_{trial+1}"
+
+                    rows = self.generate_data(experiment, trial+1)
+                    print(rows)
+            
+        # np.random.seed()
 
 
-        # with self.connect_db() as conn:
-        #     with conn.cursor() as cur:
-        #         colA = f'[1,12)'
-        #         mult = f'[2,3)'
-                
-        #         cur.execute("""
-        #         CREATE TABLE TEST_1 (
-        #             id INT GENERATED ALWAYS AS IDENTITY,
-        #             colA int4range,
-        #             mult int4range);
-                            
-
-        #         INSERT INTO TEST_1 (colA, mult) VALUES 
-        #             (%s::int4range, %s::int4range)
-        #         """, (colA, mult))
-
-
-
+    # generates pseudorandom 'fake' data based on user specified values.
     def generate_data(self, experiment :ExperimentSettings, trial: int):
         with self.connect_db() as conn:
             with conn.cursor() as cur:
@@ -72,34 +64,47 @@ class ExperimentRunner:
                     cur.execute(f"CREATE TABLE {table_name} (id INT GENERATED ALWAYS AS IDENTITY, val int4range, mult int4range);")                
                 elif experiment.data_type == DataType.SET:
                     cur.execute(f"CREATE TABLE {table_name} (id INT GENERATED ALWAYS AS IDENTITY, val int4range[], mult int4range);")
+                
+                # add reprod hash
 
                 rows = []
                 for i in range(experiment.dataset_size):
                     if experiment.data_type == DataType.RANGE:
                         val = self.generate_range(experiment)
-                        val = 'NULL' if val is None else val
+                        val = '[0,0)' if val is None else val
                     elif experiment.data_type == DataType.RANGE:
-                        val = self.generate_set(experiment)
-                        val = 'NULL' if val is None else val
+                        # val = self.generate_set(experiment)
+                        # val = 'NULL' if val is None else val
+                        val = '[0,0)' if val is None else val
+                    
+                    mult = self.generate_mult(experiment)
+                    rows.append((val, mult))    
 
+                if experiment.data_type == DataType.RANGE:
+                    template = '(%s::int4range, %s::int4range)'
+                else:
+                    template = '(%s::int4range[], %s::int4range)'
 
-    # dataset_size: int
-    # uncertain_ratio: float
-    # interval_size_range: tuple
+                sql = f"INSERT INTO {table_name} (val, mult) VALUES %s"
+                psycopg2.extras.execute_values(cur, sql, rows, template)
+
+                conn.commit()
+        return table_name
+
+    # generate an i4r. can also use psycopg.extras range type: https://www.psycopg.org/docs/extras.html#range-data-types
     def generate_range(self, experiment:ExperimentSettings) -> str:
-        # uncertain ratio. maybe should account for
-        is_uncertain = np.random.random() < experiment.uncertain_ratio
-        if (is_uncertain):
+        # uncertain ratio. maybe should account for half nulls, half mult 0
+        if np.random.random() < experiment.uncertain_ratio * 0.5:  
             return None
         
         lb = np.random.randint(experiment.interval_size_range[0], experiment.interval_size_range[1])
         ub = np.random.randint(lb+1, experiment.interval_size_range[1]+1)
-        return (lb, ub)
+        return f'[{lb}, {ub})'
     
-    def generate_set(self, experiment:ExperimentSettings) -> str:
-        num_ranges = 
+    # def generate_set(self, experiment:ExperimentSettings) -> str:
+        num_ranges = np.random.randint(*experiment.num_intervals_range)
         
-        for i in range(experiment)
+        # for i in range(experiment)
         
         # uncertain ratio. maybe should account for
         is_uncertain = np.random.random() < experiment.uncertain_ratio
@@ -108,7 +113,17 @@ class ExperimentRunner:
         
         lb = np.random.randint(experiment.interval_size_range[0], experiment.interval_size_range[1])
         ub = np.random.randint(lb+1, experiment.interval_size_range[1]+1)
-        return (lb, ub)
+        return f'array[{lb}, {ub})'
+
+    def generate_mult(self, experiment:ExperimentSettings) -> str:
+        # uncertain ratio. maybe should account for half nulls, half mult 0
+        if np.random.random() < experiment.uncertain_ratio * 0.5:  
+            upper = np.random.randint(1, experiment.mult_size_range[1]+1)
+            return f'[{0}, {upper})'
+        
+        lb = np.random.randint(experiment.mult_size_range[0], experiment.mult_size_range[1])
+        ub = np.random.randint(lb+1, experiment.mult_size_range[1]+1)
+        return f'[{lb}, {ub})'
 
     def connect_db(self):
         return psycopg2.connect(**self.db_config)
@@ -139,9 +154,9 @@ def run_all():
     runner = ExperimentRunner(db_config)
 
     # create different trials objects we want to test with different parameters modifies
-    trial1 = ExperimentSettings(name="test1", data_type=DataType.RANGE, dataset_size=10, uncertain_ratio=0.1, 
+    trial1 = ExperimentSettings(name="test1", data_type=DataType.RANGE, dataset_size=10, uncertain_ratio=0.1, mult_size_range=(1,5),
                                 interval_size_range=(1, 30), num_intervals=2, num_intervals_range=(1,3), 
-                                num_trials=2, gap_size_range=(0, 5))
+                                num_trials=2, gap_size_range=(0,5))
 
     # run the experiment for each of the trial objects.
         # inside run_experiment: 
