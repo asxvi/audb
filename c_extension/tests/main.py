dee68c7608817e275cdfa3ac54699bdf6ae0db76
asxvi <astanson02@gmail.com>
2026-01-29 13:03:18 -0600

    add in su code

--- c_extension/tests/main.py
+++ c_extension/tests/main.py
@@ -1,5 +1,6 @@
 # naming q_n1000_i2
 
+import itertools
 import pandas as pd
 import psycopg2
 import psycopg2.extras
@@ -10,8 +11,274 @@ from enum import Enum
 
 class DataType(Enum):
     RANGE = "range"
-    SET = "set"
+    SET = "rset"
 
+'''
+    local represention of postres RangeType. helper methods include arithmetic,
+    logical operators, and convenience methods
+'''
+class RangeType:
+    def __init__(self, lb=0, ub=0, isNone=False):
+        assert(lb<=ub)
+        self.lb = lb
+        self.ub = ub
+        self.isNone = isNone
+        
+    def __add__(self, o):
+        if isinstance(o, self.__class__):
+            return RangeType(self.lb+o.lb, self.ub+o.ub)
+        elif isinstance(o, int):
+            return RangeType(self.lb+o, self.ub+o)
+        else:
+            raise TypeError("unsupported operand type(s) for +: '{}' and '{}'").format(self.__class__, type(other))
+        
+    
+    def __and__(self, o):
+        if not (isinstance(self.lb, bool) and isinstance(self.ub, bool) and
+                isinstance(o.lb, bool) and isinstance(o.ub, bool)):
+            raise ValueError("Both operands must be RangeType objects with boolean bounds.")
+        
+        # Calculate the new bounds
+        new_lb = self.lb and o.lb
+        new_ub = self.ub and o.ub
+        
+        return RangeType(new_lb, new_ub)
+    
+    def __or__(self, o):
+        if not (isinstance(self.lb, bool) and isinstance(self.ub, bool) and
+                isinstance(o.lb, bool) and isinstance(o.ub, bool)):
+            raise ValueError("Both operands must be RangeType objects with boolean bounds.")
+        
+        # Calculate the new bounds
+        new_lb = self.lb or o.lb
+        new_ub = self.ub or o.ub
+        
+        return RangeType(new_lb, new_ub)
+    
+    def __mul__(self, o):
+        if isinstance(o, self.__class__):
+            lb = min(self.lb*o.lb, self.lb*o.ub, self.ub*o.lb, self.ub*o.ub)
+            ub = max(self.lb*o.lb, self.lb*o.ub, self.ub*o.lb, self.ub*o.ub)
+            return RangeType(lb, ub)
+        elif isinstance(o, int):
+            lb = min(self.lb*o, self.ub*o)
+            ub = max(self.lb*o, self.ub*o)
+            return RangeType(lb, ub)
+        else:
+            raise TypeError("unsupported operand type(s) for *: '{}' and '{}'").format(self.__class__, type(other))
+    
+    def __hash__(self):
+        # Combine the lower and upper bounds into a hashable representation
+        return hash(self.lb, self.ub)
+    
+    def __eq__(self, o):
+        return RangeType(self.lb==self.ub and self.lb==o.lb and o.lb==o.ub, self.i(o) is not None)
+
+    def __gt__(self, o):
+        return RangeType(self.lb > o.ub, self.ub > o.lb)
+
+    def __ge__(self, o):
+        return RangeType(self.lb >= o.ub, self.ub >= o.lb)
+    
+    def __lt__(self, o):
+        return RangeType(self.lb < o.ub, self.ub < o.lb)
+
+    def __le__(self, o):
+        return RangeType(self.lb <= o.ub, self.ub <= o.lb)
+
+    def u(self, o):
+        return RangeType(min(self.lb,o.lb), max(self.ub,o.ub))
+    
+    def i(self, o):
+        lb = max(self.lb,o.lb)
+        ub = min(self.ub,o.ub)
+        if lb <= ub:
+            return RangeType(max(self.lb,o.lb), min(self.ub,o.ub))
+        return None
+    
+    def __eq__(self, other):
+        if self.ub >= other.lb and other.ub >= self.lb:
+            return True
+        return False
+    
+    def __repr__(self):
+        return f"[{self.lb}, {self.ub}]"
+    
+    def __str__(self):
+        return f"[{self.lb}, {self.ub}]"
+
+    # easier to work in postgres    
+    def str_ddl(self):
+        return f"int4range({self.lb}, {self.ub})"
+    
+    # to be used only for local development and testing. 
+    def generate_range(self, experiment:ExperimentSettings) -> RangeType:
+        # uncertain ratio. maybe should account for half nulls, half mult 0
+        if np.random.random() < experiment.uncertain_ratio * 0.5:  
+            return RangeType(0,0,True)
+        
+        lb = np.random.randint(*experiment.interval_size_range)
+        ub = np.random.randint(lb+1, experiment.interval_size_range[1]+1)
+        return RangeType(lb, ub)
+
+'''
+    local represention of postres ArrayType. helper methods include arithmetic,
+    logical operators, and convenience methods
+'''
+class RangeSetType:
+    def __init__(self, rset, vtype = RangeType, cu=True):
+        assert(type(rset) is list)
+        self.vtype = vtype
+        self.rset = rset
+        if cu:
+            self.cleanup()
+        
+    def __repr__(self):
+        return f"{self.rset}"
+    
+    def __str__(self):
+        return f"{self.rset}"
+        # items = [f'"{str(i)}"' for i in self.rset]
+        # return "{" + ",".join(items) + "}"
+    
+    # easier to work in postgres    
+    def str_ddl(self):
+        items = [f"{i.str_ddl()}" for i in self.rset]
+        return "array[" + ",".join(items) + "]"
+    
+    def __len__(self):
+        return len(self.rset)
+    
+    def cleanup(self):
+        if self.vtype == RangeType:
+            if len(self)<=1:
+                return
+            l = sorted(self.rset, key=lambda r: r.lb)
+            res = []
+            curR = l[0]
+            for rv in sorted(self.rset, key=lambda r: r.lb):
+                if rv.lb <= curR.ub:
+                    curR = curR.u(rv)
+                else:
+                    res.append(curR)
+                    curR = rv
+            res.append(curR)
+            self.rset = res
+        
+    # +
+    def __add__(self, o, cu=True):
+        rst = []
+        if isinstance(o, self.__class__):
+            for p in itertools.product(self.rset,o.rset):
+                rst.append(p[0]+p[1])
+            rt = RangeSetType(rst)
+            if cu:
+                rt.cleanup()
+            return rt
+        elif isinstance(o, int):
+            for p in self.rset:
+                rst.append(p+o)
+            rt = RangeSetType(rst)
+            if cu:
+                rt.cleanup()
+            return rt
+        else:
+            raise TypeError("unsupported operand type(s) for +: '{}' and '{}'").format(self.__class__, type(other))
+        
+    # *
+    def __mul__(self, o, cu=True):
+        rst = []
+        if isinstance(o, self.__class__):
+            for p in itertools.product(self.rset,o.rset):
+                rst.append(p[0]*p[1])
+            rt = RangeSetType(rst)
+            if cu:
+                rt.cleanup()
+            return rt
+        elif isinstance(o, int):
+            for p in self.rset:
+                rst.append(p*o)
+            rt = RangeSetType(rst)
+            if cu:
+                rt.cleanup()
+            return rt
+        else:
+            raise TypeError("unsupported operand type(s) for *: '{}' and '{}'").format(self.__class__, type(other))
+
+    def __eq__(self, o):
+        lb = False
+        if len(self)==1 and len(o)==1: 
+            l = next(iter(self.rset))
+            r = next(iter(o.rset))
+            lb = l.lb==l.ub and l.lb==r.lb and r.lb==r.ub
+        return RangeType(lb, bool(self.i(o)))
+
+    def u(self, o, cu=True):
+        rst = []
+        for p in itertools.product(self.rset,o.rset):
+            rst.append(p[0].u(p[1]))
+        rt = RangeSetType(rst)
+        if cu:
+            rt.cleanup()
+        return rt
+    
+    def i(self, o, cu=True):
+        rst = []
+        for p in itertools.product(self.rset,o.rset):
+            ir = p[0].i(p[1])
+            if ir is not None:
+                rst.append(p[0].i(p[1]))
+        rt = RangeSetType(rst)
+        if cu:
+            rt.cleanup()
+        return rt
+    
+    def lb(self):
+        return sorted(self.rset, key=lambda r: r.lb)[0].lb
+    
+    def ub(self):
+        return sorted(self.rset, key=lambda r: r.ub, reverse=True)[0].ub
+    
+    def itv(self):
+        return RangeType(self.lb(),self.ub())
+    
+    def __gt__(self, o):
+        if isinstance(o, int):
+            return self.itv() > RangeType(o, o)
+        return self.itv() > o.itv()
+
+    def __ge__(self, o):
+        if isinstance(o, int):
+            return self.itv() >= RangeType(o, o)
+        return self.itv() >= o.itv()
+    
+    def __lt__(self, o):
+        if isinstance(o, int):
+            return self.itv() < RangeType(o, o)
+        return self.itv() < o.itv()
+
+    def __le__(self, o):
+        if isinstance(o, int):
+            return self.itv() <= RangeType(o, o)
+        return self.itv() <= o.itv()
+    
+    def generate_set(self, experiment:ExperimentSettings) -> RangeSetType:
+        num_ranges = np.random.randint(*experiment.num_intervals_range)
+        
+        rset = []
+        for i in range(num_ranges):    
+            # uncertain ratio. maybe should account for half nulls, half mult 0
+            if np.random.random() < experiment.uncertain_ratio * 0.5:  
+                rset.append(RangeType(0,0,True))
+                continue
+            
+            lb = np.random.randint(*experiment.interval_size_range)
+            ub = np.random.randint(lb+1, experiment.interval_size_range[1]+1)
+            
+            rset.append(RangeType(lb,ub,False))
+            print(RangeType(lb,ub,False))
+        
+        return RangeSetType(rset, cu=False)
 
 @dataclass
 class ExperimentSettings:
@@ -25,6 +292,8 @@ class ExperimentSettings:
     num_intervals_range: tuple
     num_intervals: int
     gap_size_range: tuple
+    make_csv:bool                   ####
+    insert_to_db:bool               ####
 
 class ExperimentRunner:
     def __init__(self, db_config):
@@ -92,14 +361,14 @@ class ExperimentRunner:
         return table_name
 
     # generate an i4r. can also use psycopg.extras range type: https://www.psycopg.org/docs/extras.html#range-data-types
-    def generate_range(self, experiment:ExperimentSettings) -> str:
+    def generate_range(self, experiment:ExperimentSettings) -> RangeType:
         # uncertain ratio. maybe should account for half nulls, half mult 0
         if np.random.random() < experiment.uncertain_ratio * 0.5:  
             return None
         
-        lb = np.random.randint(experiment.interval_size_range[0], experiment.interval_size_range[1])
+        lb = np.random.randint(*experiment.interval_size_range)
         ub = np.random.randint(lb+1, experiment.interval_size_range[1]+1)
-        return f'[{lb}, {ub})'
+        return RangeType(lb, ub)
     
     # def generate_set(self, experiment:ExperimentSettings) -> str:
         num_ranges = np.random.randint(*experiment.num_intervals_range)
@@ -119,9 +388,9 @@ class ExperimentRunner:
         # uncertain ratio. maybe should account for half nulls, half mult 0
         if np.random.random() < experiment.uncertain_ratio * 0.5:  
             upper = np.random.randint(1, experiment.mult_size_range[1]+1)
-            return f'[{0}, {upper})'
+            return RangeType(0, upper)
         
-        lb = np.random.randint(experiment.mult_size_range[0], experiment.mult_size_range[1])
+        lb = np.random.randint(*experiment.mult_size_range)
         ub = np.random.randint(lb+1, experiment.mult_size_range[1]+1)
         return f'[{lb}, {ub})'
 
@@ -150,34 +419,31 @@ def run_all():
         print(f"Error loading config: {e}")
         exit(1)
 
-
     runner = ExperimentRunner(db_config)
 
     # create different trials objects we want to test with different parameters modifies
     trial1 = ExperimentSettings(name="test1", data_type=DataType.RANGE, dataset_size=10, uncertain_ratio=0.1, mult_size_range=(1,5),
-                                interval_size_range=(1, 30), num_intervals=2, num_intervals_range=(1,3), 
+                                interval_size_range=(1, 1000), num_intervals=2, num_intervals_range=(1,3), make_csv=False, insert_to_db=True,
                                 num_trials=2, gap_size_range=(0,5))
 
+    trial2 = ExperimentSettings(name="test2", data_type=DataType.RANGE, dataset_size=17, uncertain_ratio=0.3, mult_size_range=(1,5),
+                                interval_size_range=(1, 100), num_intervals=2, num_intervals_range=(1,3), make_csv=False, insert_to_db=True,
+                                num_trials=2, gap_size_range=(0,5))
+    
     # run the experiment for each of the trial objects.
         # inside run_experiment: 
             # for every trial in Trial
             # generate data (with reproducibilty)
-    runner.run_experiment(trial1)
+    # runner.run_experiment(trial1)
+    # runner.run_experiment(trial2)   
 
 
+def create_experiment_name(experiment: ExperimentSettings):
+    dtype = 'r' if experiment.data_type == DataType.RANGE else 's'
+    # seed = np.random.seed()
+    return f"t_{experiment.name}_{dtype}_n{experiment.dataset_size}"
 
-
-
-    # change up params, creating new experiment config
-
-# name: str
-# dataset_size: int
-# uncertain_ratio: float
-# interval_size_range: tuple
-# num_intervals: int
-# num_intervals_range: tuple
-# num_trials: int
-# gap_size_range: tuple
+    # change up params, creating new experiment config...
 
 if __name__ == '__main__':
     run_all()
